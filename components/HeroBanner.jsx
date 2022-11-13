1f2baca704d13268596f71c1b1a2e716329d42a3
Muhammad Bilawal <bilawal225@gmail.com>
2022-11-13 21:33:38 +0500

    convert js master into bit-apparels

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { urlFor } from '../lib/client';
+import { urlFor } from "../lib/client";
 
 const HeroBanner = ({ heroBanner }) => {
+  console.log(heroBanner);
   return (
     <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
 
         <div>
           <Link href={`/product/${heroBanner.product}`}>
@@ -23,7 +28,7 @@ const HeroBanner = ({ heroBanner }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
